e4ce537f0f44ef0878c5e16f111821b62516cf38
tea444 <tbubic00@fesb.hr>
2023-10-16 16:53:34 +0200

    Partly finished lab1

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -1,8 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE (50)
+#define MAX_LINE (1024)
+#define FILE_ERROR_OPEN (-1)
+#define EXIT_SUCCES (0)
+
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
 
 int main() {
 
-	printf("Hello world");
+	int noRows = 0;
+	FILE* filepointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
 
-	return 0;
+	filepointer = fopen("students.txt", "r");
+	if (!filepointer) {
+		printf("File is not opened!\n");
+			return FILE_ERROR_OPEN;
+	}
+
+	fclose(filepointer);
+	return EXIT_SUCCES;
+
 }
