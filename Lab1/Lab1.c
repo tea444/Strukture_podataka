b55499077c5c4d9390c020908c3d1aa0bbbd7e16
tea444 <tbubic00@fesb.hr>
2023-10-28 21:12:28 +0200

    Finished Lab1

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -1,7 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS //mora bit PRVA LINIJA KODA!
+/*
+1. Napisati program koji prvo proèita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinamièki alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i uèitati iz datoteke sve zapise. Na ekran ispisati ime,
+prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100
+*/
+
+
+#define _CRT_SECURE_NO_WARNINGS //mora uvijek biti PRVA LINIJA KODA!
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define MAX_SIZE (50)
 #define MAX_LINE (1024)
 #define FILE_ERROR_OPEN (-1)
@@ -13,26 +24,25 @@ typedef struct _student {
 	double points;
 } Student;
 
-int readNoRowsInFile();
+int readNoRowsInFile(void);
 Student* allocateMemoryAndReadStudents(int);
 int showStudents(int, Student*);
 
 int main() {
 
 	int noRows = 0;
-	Student* students = NULL;
+	Student* student = NULL;
 
-	noRows=ReadNoRowsInFile();
+	printf("Broj redova, tj. studenata zapisanih u datoteci je: %d\n", noRows);
+
+	noRows=readNoRowsInFile();
 
 	if (noRows > 0) {
-		students = allocateMemoryAndReadStudents();
-		showStudents(noRows, students);
+		student=allocateMemoryAndReadStudents(noRows);
+		showStudents(noRows, student);
 
-		free(students);
+		free(student);
 	}
-
-	printf("%d", noRows);
-
 
 		return EXIT_SUCCESS;
 }
@@ -40,53 +50,62 @@ int main() {
 int readNoRowsInFile() {
 
 	int counter = 0;
-	FILE* filePointer = NULL;
+	FILE* fp = NULL;
 	char buffer[MAX_LINE] = { 0 };
 
-	filePointer = fopen("students.txt", "r");
-	if (!filePointer) {
-		printf("File is not opened!\n");
+	fp = fopen("students.txt", "r");
+	if (!fp) {
+		printf("File can't open!\n");
 		return FILE_ERROR_OPEN;
 	}
 
-	while (!feof(filePointer)) { // character po character
-		fgets(buffer, MAX_LINE, filePointer); //za èitanje reda, 1. buffer, 2. max count, 3. file string 
-		counter++;								// buffer radi array koji broji bro linija i sprema u char
+	while (!feof(fp)) { // character po character
+		fgets(buffer, MAX_LINE, fp); //za èitanje reda, 1. buffer, 2. max count, 3. file string 
+		counter++;								
 	}
 
-	fclose(filePointer);
-	return counter;
+	fclose(fp);
+	return counter;  //vraæamo broj redova
 }
 
 Student* allocateMemoryAndReadStudents(int noStudents) {
 	int counter = 0;
-	FILE* filePointer = NULL;
+	FILE* fp = NULL;
 	Student* students = NULL;
 
 	students = (Student*)malloc(noStudents * sizeof(Student));
 	if (!students) {
-		printf("Neuspjesna alokacij amemorije!\n");
+		printf("Error allocation!\n");
 		return NULL;
 	}
 
-	filePointer = fopen("students.txt", "r");
-	if (!filePointer)
+	fp = fopen("students.txt", "r");
+	if (!fp)
 	{
-		printf("Neuspjesno otvaranje datoteke!\n");
+		printf("File can't open!\n");
 		return FILE_ERROR_OPEN;
 	}
 
-	while (!feof(filePointer)) {
+	while (!feof(fp)) {
 
-		fscanf(filePointer, " %s %s %lf", students[counter].name, students[counter].surname, students[counter].points);
+		fscanf(fp, "%s %s %lf", students[counter].name, students[counter].surname, &students[counter].points);
 		counter++;
 	}
 
-	fclose(filePointer);
+	fclose(fp);
+	
+	return students;
 }
 
-int showStudnets(int noStudents, Student* students) {
+int showStudents(int noStudents, Student* students) {
 
+	int i = 0;
 
+	for (i = 0;i < noStudents;i++) {
 
-}
+		printf("Ime: %s\t Prezime: %s\t Broj bodova i relativan broj bodova: %lf %lf \n", students[i].name, students[i].surname, students[i].points, students[i].points/MAX_POINTS*100);
+	}
+
+	return EXIT_SUCCESS;
+
+} 
--- Lab2/Source.c
+++ Lab2/Source.c
@@ -9,6 +9,7 @@ U zadatku se ne smiju koristiti globalne varijable. */
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define MAX_SIZE (50)
 
@@ -21,10 +22,46 @@ typedef struct _person {
 	Position next;
 }Person;
 
+Position createPerson();
+int addToFront(Position);
+
 int main() {
 	Person head = { .ime = {0},.prezime = {0}, .birthYear=0, .next=NULL };
 
 
 	return EXIT_SUCCESS;
+
+}
+Position createPerson() {
+	Position newPerson = NULL;
+	char ime[MAX_SIZE] = { 0 };
+	char prezime[MAX_SIZE] = { 0 };
+	int birthYear = 0;
+
+	newPerson = (Position)malloc(sizeof(Person));
+
+	//Provjera
+
+	printf("Unesi ime\n");
+	scanf("%s", ime);
+
+	// -||-
+
+	strcpy(newPerson->ime, ime); //ime je pokazivac za ime[max size]
+
+	// -||-
+
+	return newPerson;
+
+}
+
+int addToFront(Position head) {
+	Position newPerson = NULL;
+	newPerson = createPerson();
+
+	if (newPerson) {
+		newPerson->next = head->next;
+		head->next = newPerson;
+	}
 
 }
